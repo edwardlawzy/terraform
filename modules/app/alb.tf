a420d1c53d825edb3ebcc9c5d517279517198ef0
edwardlawzy <edwardlawzy@gmail.com>
2025-10-03 23:23:42 +0300

    fix variables

--- modules/app/alb.tf
+++ modules/app/alb.tf
@@ -1,4 +1,4 @@
-resource "aws_alb" "alb" {
+resource "aws_lb" "alb" {
     name               = "${var.project_name}-alb"
     internal           = false
     load_balancer_type = "application"
@@ -6,14 +6,14 @@ resource "aws_alb" "alb" {
     security_groups    = [var.asg_sg]
 }
 
-resource "aws_alb_target_group" "alb-tg" {
+resource "aws_lb_target_group" "alb-tg" {
       name     = "${var.project_name}-alb-tg"
       port     = 80
       protocol = "HTTP"
       vpc_id   = var.vpc_id
 
       health_check {
-        path = "/"
+        path = "/health.php"
         port = "80"
         # protocol            = "HTTP"
         # matcher             = "200"
@@ -25,16 +25,22 @@ resource "aws_alb_target_group" "alb-tg" {
 
 }
 
-resource "aws_alb_listener" "alb-listener" {
-    load_balancer_arn = aws_alb.alb.arn
+resource "aws_lb_listener" "alb-listener" {
+    load_balancer_arn = aws_lb.alb.arn
     port              = 80
     protocol          = "HTTP"
 
     default_action {
         type             = "forward"
-        target_group_arn = aws_alb_target_group.alb-tg.arn
+        target_group_arn = aws_lb_target_group.alb-tg.arn
     }
 }
+
+# resource "aws_lb_target_group_attachment" "example_attachment" {
+#   target_group_arn = aws_lb_target_group.alb-tg.arn
+#   target_id        = aws_instance.web_server.id
+#   port             = 80 # Optional: Specify if different from target group's default port
+# }
 
 # resource "aws_alb_listener_rule" "alb-listener-rule" {
 #     listener_arn = aws_alb_listener.alb-listener.arn
