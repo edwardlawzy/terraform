c378ae7753e21e728bb0ff21713c645e3899162f
edwardlawzy <edwardlawzy@gmail.com>
2025-10-03 23:07:16 +0300

    fix variables

--- modules/app/alb.tf
+++ modules/app/alb.tf
@@ -13,13 +13,14 @@ resource "aws_alb_target_group" "alb-tg" {
       vpc_id   = var.vpc_id
 
       health_check {
-        path                = "/"
-        protocol            = "HTTP"
-        matcher             = "200"
-        interval            = 30
-        timeout             = 5
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
+        path = "/"
+        port = "80"
+        # protocol            = "HTTP"
+        # matcher             = "200"
+        # interval            = 30
+        # timeout             = 5
+        # healthy_threshold   = 2
+        # unhealthy_threshold = 2
       }
 
 }
