509cef25ee1e01838db4eef36e94c4602a9c2950
edwardlawzy <edwardlawzy@gmail.com>
2025-10-18 14:12:02 +0300

    add bastion host to eks

--- modules/eks/bastion.tf
+++ modules/eks/bastion.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "bastion" {
   ami                    = var.ami_type
   instance_type          = "t3.nano"
-  subnet_id              = [var.bastion_subnet_ids]
+  subnet_id              = var.public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   key_name               = var.keypair_name
   associate_public_ip_address = true
