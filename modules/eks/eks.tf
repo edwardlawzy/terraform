d59eb84c3a16bf8af2c017f5cf8cbc23d47b136e
edwardlawzy <edwardlawzy@gmail.com>
2025-10-18 19:02:10 +0300

    add bastion host to eks

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -17,24 +17,7 @@ resource "aws_iam_role" "eks" {
 POLICY
 }
 
-resource "aws_iam_role" "eks-bastion-role" {
-  name = "eks-bastion-role"
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      }
-    }
-  ]
-}
-POLICY
-}
 
 
 
@@ -43,27 +26,7 @@ resource "aws_iam_role_policy_attachment" "eks" {
   role       = aws_iam_role.eks.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks-bastion-policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-bastion-role.name
-}
 
-resource "aws_eks_access_entry" "bastion" {
-  cluster_name    = aws_eks_cluster.eks.name
-  # The ARN of the IAM User or Role you want to grant access to
-  principal_arn   = aws_iam_role.eks-bastion-role.arn
-  type            = "EC2" 
-}
-
-resource "aws_eks_access_policy_association" "bastion" {
-  cluster_name = aws_eks_cluster.eks.name
-  principal_arn = aws_eks_access_entry.bastion.principal_arn
-  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-  
-  access_scope {
-    type = "cluster"
-  }
-}
 
 resource "aws_eks_cluster" "eks" {
   name     = "${var.project_name}-eks-cluster"
